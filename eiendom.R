b5dd9f4f0f4e9a70cc506cba30d44268d1ff5c68
Chris A. Stokvik <cst040@post.uit.no>
2018-11-19 22:19:02 +0100

    link ssb boligpris

---
+++ eiendom.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(readxl)
+
+
+boligpris <- read.csv("http://data.ssb.no/api/v0/dataset/25151.csv?lang=no")
+
+
+boligpris
+
+### De ble ikke korrekt justert, sjekk arbeidskravet hvor vi skulle laste hotelovernatting hvordan jeg gjorde det, evnt DC import (2) 
+
