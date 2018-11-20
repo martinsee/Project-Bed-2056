fe1b341f5abcc16c230dcddef034de64d88e1c9b
Chris A. Stokvik <cst040@post.uit.no>
2018-11-20 07:27:30 +0100

    fikset statistikk variabel kolonne

--- eiendom.R
+++ eiendom.R
@@ -6,14 +6,8 @@ library(tidyr)
 
 boligpris <- read.csv("http://data.ssb.no/api/v0/dataset/25151.csv?lang=no", sep = ";", stringsAsFactors = TRUE)
 
-?read.csv
+# Justere data slik at man sprer kolonnen statistikk variabel 
+
+boligpris <- spread(boligpris, statistikkvariabel , Gjennomsnittlig.kvadratmeterpris..etter.region..type.enebolig...r.og.statistikkvariabel)
 
 
-boligpris
-
-### De ble ikke korrekt justert, sjekk arbeidskravet hvor vi skulle laste hotelovernatting hvordan jeg gjorde det, evnt DC import (2) 
-
-
-boligpris <- spread(boligpris, statistikkvariabel , Gjennomsnittlig.kvadratmeterpris..etter.region.type.enebolig...r.og.statistikkvariabel, )
-
-?spread
