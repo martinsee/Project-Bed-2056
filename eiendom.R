1646d6337667242e3c4a8d2c71a4f6d1afa57797
Chris A. Stokvik <cst040@post.uit.no>
2018-11-20 08:36:21 +0100

    La inn kommentar til meg sev

--- eiendom.R
+++ eiendom.R
@@ -11,3 +11,8 @@ boligpris <- read.csv("http://data.ssb.no/api/v0/dataset/25151.csv?lang=no", sep
 boligpris <- spread(boligpris, statistikkvariabel , Gjennomsnittlig.kvadratmeterpris..etter.region..type.enebolig...r.og.statistikkvariabel)
 
 
+boligpris <- boligpris %>%
+  rename(Year = "Dato")
+ 
+# 20.11 innser nå at data er i år men resten er å måneder, skal sjekke om jeg finner andre kilder, 
+# eller har jeg lastet opp feil? trudde de var kvartalsmessig, sjekk ssb 
