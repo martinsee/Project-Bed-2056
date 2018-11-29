bee29da669504f5e18f257ab26290fa44dcf6174
Martin Bjørnerem <martinsbjornerem@gmail.com>
2018-11-29 22:41:34 +0100

    added houseprices and joined to quarterly dataframe, and added sd to monthly

--- osebx.R
+++ osebx.R
@@ -2,6 +2,20 @@ library(tidyverse)
 library(readxl)
 library(lubridate)
 library(mosaic)
+library(formattable)
+
+#Henter data for eiendomspriser
+oecd.hus <- read_csv("DP_LIVE_27112018111628959.csv")
+
+oecd.hus <- oecd.hus %>%
+  filter(SUBJECT == "NOMINAL", FREQUENCY == "Q") %>%
+  select(TIME, Value)
+
+oecd.hus$TIME <- oecd.hus$TIME %>%
+  str_replace_all(c("Q1" = "01-01", "Q2" = "04-01", "Q3" = "07-01", "Q4" = "10-01")) %>%
+  ymd()
+
+names(oecd.hus) <- c("Dato", "Bolig")
 
 #Hente kursdata for equinor
 eqnr <- read_csv("https://www.netfonds.no/quotes/paperhistory.php?paper=EQNR.OSE&csv_format=csv",
@@ -38,11 +52,11 @@ bitcoin <- bitcoin %>%
 
 bitcoin$Dato <- mdy(bitcoin$Dato)
 
-head(bitcoin) #tidligste dato er 2. feb 2012 
-tail(bitcoin) 
+bitcoin <- bitcoin %>% group_by(Dato=floor_date(Dato, "month")) %>%
+  summarise(BTC_usd = mean(btc_usd))
 
 #Henter prisdata for gull i USD
-gull <- read_csv("data/monthly_csv.csv")                      #kanskje oppdateres?
+gull <- read_csv("data/monthly_csv.csv")                   #kanskje oppdateres?
 
 names(gull) <- c("Dato", "Gull_usd")
 gull$Dato <- parse_date_time(gull$Dato, orders = "Ym")
@@ -60,7 +74,6 @@ usd_nok$Dato <- usd_nok$Dato %>%
   as.character() %>%
   parse_date_time(.,orders = "Omy")
 
-#Joiner dataene med left_join på dato og fjerner de som ikke har match.lurt?
 #akkurat nå er enkelte class 'POSIXct' og andre 'Date', gjør om POSIXct til Date
 gull$Dato <- as.Date(gull$Dato)
 usd_nok$Dato <- as.Date(usd_nok$Dato)
@@ -70,18 +83,32 @@ monthly_joined <- monthly_osebx %>%
   inner_join(usd_nok, by = "Dato") %>%
   inner_join(monthly_eqnr, by = "Dato")
 
+quarterly_joined <- oecd.hus %>%
+  left_join(monthly_osebx, by = "Dato") %>% 
+  left_join(gull, by = "Dato") %>% 
+  left_join(usd_nok, by = "Dato") %>%
+  left_join(monthly_eqnr, by = "Dato")
+
 full <- osebx %>%
   full_join(gull, by = "Dato") %>% 
   full_join(usd_nok, by = "Dato") %>%
-  full_join(eqnr, by = "Dato")
-
-#' monthly_joined <- monthly_joined %>%
-#'left_join(bitcoin, by = "Dato") #egen på grunn av få datoer + daglige vs månedlige
+  full_join(eqnr, by = "Dato") %>%
+  full_join(oecd.hus, by = "Dato") %>%
+  full_join(bitcoin, by = "Dato")
 
 monthly_joined <- monthly_joined %>%
- mutate(Gull_nok = Gull_usd*USD_NOK)#, BTC_nok = btc_usd*usd_kurs)
+ mutate(Gull_nok = Gull_usd*USD_NOK)
+
+bitcoin_joined <- bitcoin %>% #egen på grunn av få måneder med data
+  left_join(usd_nok, by = "Dato") %>%
+  mutate(BTC_nok = BTC_usd*USD_NOK, returns = c(NA, diff(log(BTC_nok)))) %>%
+  na.omit() %>%
+  mutate(cumsum = cumsum(returns))
 
 monthly_gathered <- monthly_joined %>%
+  gather(key = "investering", value = "verdi",-Dato)
+
+quarterly_gathered <- quarterly_joined %>%
   gather(key = "investering", value = "verdi",-Dato)
 
 summary(monthly_gathered)
@@ -91,22 +118,57 @@ monthly_gathered <- monthly_gathered %>%
 monthly_gathered$investering <- as.factor(monthly_gathered$investering)
 monthly_gathered$verdi <- as.numeric(monthly_gathered$verdi)
 
-summary(monthly_gathered)
+summary(monthly_gathered) #sjekker for NA
 
-ggplot(monthly_gathered, aes(x=Dato, y=verdi, col = investering)) + geom_line()
-
-logs <- monthly_gathered %>%
+monthly_returns <- monthly_gathered %>%
   group_by(investering) %>%
   arrange(investering, Dato) %>%
-  mutate(returns = c(NA, diff(log(verdi))))
+  mutate(returns = c(NA, exp(diff(log(verdi)))-1)) %>%
+  na.omit()
 
-logs <- logs %>%
-  na.omit() %>%
+summary(monthly_returns) 
+
+quarterly_returns<- quarterly_gathered %>%
+  group_by(investering) %>%
+  arrange(investering, Dato) %>%
+  mutate(returns = c(NA, exp(diff(log(verdi)))-1)) %>%
+  na.omit()
+
+cumsum2 <- quarterly_returns %>%
   group_by(investering) %>%
   mutate(cum_returns = cumsum(returns))
 
-ggplot(logs, aes(x=Dato, y= cum_returns, col = investering)) + geom_line()
+ggplot(cumsum2, aes(x=Dato, y= cum_returns, col = investering)) + geom_line()
 
+
+monthly_returns %>% 
+  group_by(investering, year=floor_date(Dato, "year")) %>%
+  summarise(sd = sd(returns))
+
+monthly_returns %>% 
+  group_by(investering, year=floor_date(Dato, "year")) %>%
+  summarise(sum = sum(returns))
+
+sum <- monthly_returns %>%
+  group_by(investering, year=floor_date(Dato, "year")) %>%
+  summarise(total_returns = sum(returns), mean_price = mean(verdi), sd_price = sd(verdi), mean_returns = mean(returns), sd_returns = sd(returns)) %>%
+  arrange(year)
+#sd på verdi eller returns? kan de sammenlignes
+
+sum$total_returns <- percent(sum$total_returns)
+sum$mean_returns <- percent(sum$mean_returns)
+sum$sd_returns <- percent(sum$sd_returns)
+
+cumsum <- monthly_returns %>%
+  group_by(investering) %>%
+  mutate(cum_returns = cumsum(returns))
+
+ggplot(cumsum, aes(x=Dato, y= cum_returns, col = investering)) + geom_line()
+
+ggplot()+
+geom_line(data = cumsum, aes(x=Dato, y= cum_returns, col = investering)) +
+geom_line(data = bitcoin_joined, aes(x=Dato, y=cumsum, col = "BTC"))
+# stemmer returns på bitcoin, skal ha gått ned med 75% siden topp.
 
 #Samvariasjon mellom equinor hovedindeksen?
 fit <- lm(OSEBX~EQNR, data=monthly_joined)
@@ -133,4 +195,3 @@ cor(monthly_joined$Gull_nok, monthly_joined$USD_NOK)
 fit3 <- lm(BTC_nok~USD_NOK, data=monthly_joined)
 summary(fit3)
 cor(monthly_joined$BTC_nok, monthly_joined$usd_kurs)
-#na.rm unused argument?
