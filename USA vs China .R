fdb7c0dcda87cb5a30a0106b0f53c081937264a8
Chris A. Stokvik <cst040@post.uit.no>
2018-12-09 12:37:25 +0100

    oppådatert data

--- USA vs China .R
+++ USA vs China .R
@@ -1,11 +1,10 @@
-#### The trade war comparison 
+## The trade war comparison 
 
-cod <- ("2011-01-03")
+cod <- ("2002-01-03")
 
+## Oil wti 
 
-### Oil wti 
-
-oilwti <- read_csv("data/Crude Oil WTI  Historical Data.csv", 
+oilwti <- read_csv("data/Crude Oil WTI Futures Historical Data.csv", 
                    col_types = cols(Date = col_date(format = "%b %d, %Y")))
 
 oilwti <- oilwti %>%
@@ -13,7 +12,7 @@ oilwti <- oilwti %>%
   filter(Date >= as.Date(cod))
 
 
-#### USA 3m Bond rate
+## USA 3m Bond rate
 
 usa3m <- read_csv("data/United States 3-Month Bond Yield Historical Data.csv", 
                   col_types = cols(Date = col_date(format = "%b %d, %Y")))
@@ -23,17 +22,17 @@ usa3m <- usa3m %>%
   filter(Date >= as.Date(cod))
 
 
-### Dow J. indeks 
+## Dow J. indeks 
 
-dowj <- read_csv("data/Dow Jones Industrial Average 1970-2018.csv",
-                 col_types = cols(Date = col_date(format = "%Y-%m-%d")))
+dowj <- read_csv("data/Dow Jones Industrial Average Historical Data.csv",
+                 col_types = cols(Date = col_date(format = "%b %d, %Y")))
 
 dowj <- dowj %>%
-  select(Date, Close) %>%
+  select(Date, Price) %>%
   filter(Date >= as.Date(cod))
 
 
-### Shanghai, fra 2011. 
+## Shanghai, fra 2011. 
 
 
 shanghai <- read_csv("data/Shanghai Composite Historical Data.csv", 
@@ -46,7 +45,7 @@ shanghai <- shanghai %>%
 
 
 
-#### Shenzhen 
+## Shenzhen 
 
 shen <- read_csv("data/SZSE Component Historical Data.csv", 
                                            col_types = cols(Date = col_date(format = "%b %d, %Y")))
@@ -56,7 +55,7 @@ shen <- shen %>%
   filter(Date >= as.Date(cod))
 
 
-#### China 1Y Bond 
+## China 1Y Bond 
 
 china_1y <- read_csv("data/China 1-Year Bond Yield Historical Data.csv", 
                                                     col_types = cols(Date = col_date(format = "%b %d, %Y")))
@@ -64,6 +63,37 @@ china_1y <- read_csv("data/China 1-Year Bond Yield Historical Data.csv",
 china_1y <- china_1y %>%
   select(Date, Price) %>%
   filter(Date >= as.Date(cod))
+
+## Bitcoin 
+
+bitcoin_day <- read_csv("data/BTC_USD Bitfinex Historical Data.csv", 
+                        col_types = cols(Date = col_date(format = "%b %d, %Y")))
+
+bitcoin_day <- bitcoin_day %>%
+  select(Date, Price) %>%
+  rename(Close = "Price") %>%
+  filter(Date >= as.Date(cod))  
+
+
+## Nasdaq 
+
+nasdaq <- read_csv("data/NASDAQ Composite Historical Data.csv", 
+                   col_types = cols(Date = col_date(format = "%b %d, %Y")))
+
+nasdaq <- nasdaq %>%
+  select(Date, Price) %>%
+  filter(Date >= as.Date(cod))
+
+
+## Xpr 
+
+xpr_day <- read_csv("data/XRP Historical Data - Investing.com.csv", 
+                    col_types = cols(Date = col_date(format = "%b %d, %Y")))
+
+xpr_day <- xpr_day %>%
+  select(Date, Price) %>%
+  rename(Close = "Price") %>%
+  filter(Date >= as.Date(cod))  
 
 
 #### Felles datasett 
@@ -75,14 +105,19 @@ UC <- china_1y %>%
   full_join(shen, by = "Date") %>%
   full_join(usa3m, by = "Date") %>%
   full_join(shanghai, by = "Date") %>%
-  rename(china_bond = "Price.x", dowj = "Close",  oilwti = "Price.y", shen = "Price.x.x", usa_bond = "Price.y.y", shanghai = "Price" ) %>%
-  na.omit()
+  full_join(xpr_day, by = "Date") %>% 
+  full_join(bitcoin_day, by = "Date") %>% 
+  full_join(nasdaq, by = "Date")%>%
+  rename(china_bond = "Price.x", dowj = "Close.x", oilwti = "Price.y", shen = "Price.x.x", usa_bond = "Price.y.y", shanghai = "Price",
+         Ripple = "Close.x.x", Bitcoin = "Close.y", Nasdaq = "Close.y.y")
+
+
 
 ## Gjøre data om til lang    
   
 
 UC_long  <- UC %>% 
-  select(Date, china_bond, dowj, oilwti, shen, usa_bond, shanghai) %>%
+  select(Date, china_bond, dowj, oilwti, shen, usa_bond, shanghai, Ripple, Bitcoin, Nasdaq) %>%
   gather(key= "asset", value = "Close", -Date)
 
 ## Lage plot 
@@ -98,7 +133,7 @@ ggplot(UC_long, aes(x=Date, y=Close, col=asset ))+
 library(corrplot)
 
 UC_cor  <- UC %>% 
-  select(china_bond, dowj, oilwti, shen, usa_bond, shanghai)
+  select(china_bond, dowj, oilwti, shen, usa_bond, shanghai, Ripple, Bitcoin, Nasdaq)
 
 res <- cor(UC_cor)
 round(res, 2)
@@ -106,5 +141,36 @@ round(res, 2)
 corrplot(res, type = "upper", order = "hclust", 
          tl.col = "black", tl.srt = 45)
 
+#### Lengre tids analyse uten krypto
+
+UC_u <- china_1y %>%
+  full_join(oilwti, by = "Date") %>%
+  full_join(usa3m, by = "Date") %>%
+  full_join(shanghai, by = "Date") %>%
+  full_join(nasdaq, by = "Date") %>% 
+  rename( china_bond = "Price.x",  oilwti = "Price.y", usa_bond = "Price.x.x", shanghai = "Price.y.y", nasdaq = "Price") %>% 
+  na.omit()
+
+
+UC_u_long  <- UC_u %>% 
+  select(Date, china_bond, oilwti,  usa_bond, shanghai, nasdaq) %>%
+  gather(key= "asset", value = "Close", -Date)
+
+## Lage plot 
+
+ggplot(UC_u_long, aes(x=Date, y=Close, col=asset ))+
+  geom_line()+
+  scale_y_log10(name = "Close")+
+  ggtitle("USA og Kina") 
+
+
+UC_u_cor  <- UC_u %>% 
+  select(china_bond, oilwti, usa_bond, shanghai, nasdaq)
+
+res <- cor(UC_u_cor)
+round(res, 2)
+
+corrplot(res, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
 
 
